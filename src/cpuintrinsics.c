ec0d465f9153afc74959470d3e6d20ee9a122c12
Felipe Noronha <felipenoris@gmail.com>
2019-03-12 00:15:36 -0300

    _rdrand32_step

--- src/cpuintrinsics.c
+++ src/cpuintrinsics.c
@@ -1,8 +1,17 @@
 
 #include <stdio.h>
+#include <immintrin.h>
+
+unsigned int randuint()
+{
+	unsigned int result;
+	if (_rdrand32_step(&result))
+		return result;
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
-	printf("Hello world!\n");
+	printf("Random number: %u \n", randuint());
 }
 
